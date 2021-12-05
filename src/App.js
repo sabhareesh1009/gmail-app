18515a1d7200362b6f847e8190f33877bf9848b3
sabhareesh1009 <sabhareeshkandikonda1109@gmail.com>
2021-12-05 22:47:46 +0530

    Added the header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+      <Header />
+      <div className="app__body">
+        <Sidebar />
+      </div>
     </div>
   );
 }
